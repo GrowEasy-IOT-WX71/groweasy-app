93b5332b74c18ff283f16eb936195092fca2603a
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-09-25 10:02:52 -0500

    First version

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD2_uQFV61azVkZSTCL2FFtO1tx_tc0_Zw",
+    authDomain: "groweasy-iot.firebaseapp.com",
+    projectId: "groweasy-iot",
+    storageBucket: "groweasy-iot.appspot.com",
+    messagingSenderId: "7910115005",
+    appId: "1:7910115005:web:819f3ae262bcbea22799d4",
+    measurementId: "G-TVTS9HM94H"
+  },
+};
